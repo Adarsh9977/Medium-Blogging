6fe4df025be3523d15cf5b7155224296e82fbd84
Adarsh Tiwari <134617221+Adarsh9977@users.noreply.github.com>
2024-08-13 08:49:33 +0530

    Update Blog.tsx

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 export const Blog = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const {loading, blog} = useBlog({
     id : Number(id)
